a27e2e578b3c5099e4b5d0bb1557d2554a996ee2
Nefos1516 <d.netelev03@gmail.com>
2023-11-29 02:17:34 +0300

    fixed type casts

--- reduce_auth.py
+++ reduce_auth.py
@@ -13,12 +13,12 @@ for line in sys.stdin:
     if old_node_id is not None and old_node_id != node_id:
         print(f'{old_node_id}\t{auth_score}\t{hub_score}\t{";".join(links)}')
         norm += auth_score ** 2
-        auth_score, hub_score = 0.0
+        auth_score, hub_score = 0.0, 0.0
         links = None
     if len(kv) > 3:
         links = kv[3].split(';')
         hub_score = kv[2]
     else:
         auth_score += float(kv[2])
-os.environ['NORM'] = math.sqrt(norm)
+os.environ['NORM'] = str(math.sqrt(norm))
 print(f'{node_id}\t{auth_score}\t{hub_score}\t{";".join(links)}')
--- reduce_hub.py
+++ reduce_hub.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import math
 import sys
 import os
 
@@ -12,12 +13,12 @@ for line in sys.stdin:
     if old_node_id is not None and old_node_id != node_id:
         print(f'{old_node_id}\t{auth_score}\t{hub_score}\t{";".join(links)}')
         norm += hub_score ** 2
-        auth_score, hub_score = 0.0
+        auth_score, hub_score = 0.0, 0.0
         links = None
     if len(kv) > 3:
         links = kv[3].split(';')
         auth_score = kv[1]
     else:
         hub_score += float(kv[1])
-os.environ['NORM'] = norm
+os.environ['NORM'] = str(math.sqrt(norm))
 print(f'{node_id}\t{auth_score}\t{hub_score}\t{";".join(links)}')
