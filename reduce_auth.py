39b78f1f33d5031ad675c613dce74add1077f9c5
Nefos1516 <d.netelev03@gmail.com>
2023-11-29 01:41:51 +0300

    fixed env var and itr vars

--- reduce_auth.py
+++ reduce_auth.py
@@ -3,10 +3,8 @@ import sys
 import os
 
 norm = 0
-old_node_id = None
-node_id = None
-links = None
-auth_score, hub_score = 0.0
+old_node_id, node_id, links = None, None, None
+auth_score, hub_score = 0.0, 0.0
 for line in sys.stdin:
     kv = line.strip().split('\t')
     old_node_id = node_id
--- runPy.sh
+++ runPy.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 itr_count=3
-hdfs dfs -cmdenv NORM=1
-for ((itr=1; itr <= $itr_count; itr++)); do
+itr = 1
+
+echo "Doing iteration $itr of $itr_count..."
+hdfs dfs -rm -r PR/itr_$((itr))/hub
+yarn jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-*.jar \
+    -cmdenv NORM=1 \
+    -D mapreduce.job.name="PageRank Job via Streaming" \
+    -files $(pwd)/map_auth.py,$(pwd)/reduce_auth.py \
+    -input PR/itr_$itr/auth \
+    -output PR/itr_$((itr))/hub \
+    -mapper "python `pwd`/map_auth.py" \
+    -reducer "python `pwd`/reduce_auth.py"
+hdfs dfs -rm -r PR/itr_$((itr+1))/auth
+yarn jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-*.jar \
+    -D mapreduce.job.name="PageRank Job via Streaming" \
+    -files $(pwd)/map_hub.py,$(pwd)/reduce_hub.py \
+    -input PR/itr_$itr/hub \
+    -output PR/itr_$((itr+1))/auth \
+    -mapper "python `pwd`/map_hub.py" \
+    -reducer "python `pwd`/reduce_hub.py"
+for ((itr=2; itr <= $itr_count; itr++)); do
     echo "Doing iteration $itr of $itr_count..."
     hdfs dfs -rm -r PR/itr_$((itr))/hub
     yarn jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-*.jar \
