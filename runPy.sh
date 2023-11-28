2e02fcf6b4c90009ad4d1211912477f8b5cf60b7
Nefos1516 <d.netelev03@gmail.com>
2023-11-29 01:25:19 +0300

    fixed issues with unpack variables and streaming

--- runPy.sh
+++ runPy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 itr_count=3
+hdfs dfs -cmdenv NORM=1
 for ((itr=1; itr <= $itr_count; itr++)); do
     echo "Doing iteration $itr of $itr_count..."
     hdfs dfs -rm -r PR/itr_$((itr))/hub
@@ -8,15 +9,15 @@ for ((itr=1; itr <= $itr_count; itr++)); do
         -files $(pwd)/map_auth.py,$(pwd)/reduce_auth.py \
         -input PR/itr_$itr/auth \
         -output PR/itr_$((itr))/hub \
-        -mapper $(pwd)/map_auth.py \
-        -reducer $(pwd)/reduce_auth.py
+        -mapper "python `pwd`/map_auth.py" \
+        -reducer "python `pwd`/reduce_auth.py"
     hdfs dfs -rm -r PR/itr_$((itr+1))/auth
     yarn jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-*.jar \
         -D mapreduce.job.name="PageRank Job via Streaming" \
         -files $(pwd)/map_hub.py,$(pwd)/reduce_hub.py \
         -input PR/itr_$itr/hub \
         -output PR/itr_$((itr+1))/auth \
-        -mapper $(pwd)/map_hub.py \
-        -reducer $(pwd)/reduce_hub.py
+        -mapper "python `pwd`/map_hub.py" \
+        -reducer "python `pwd`/reduce_hub.py"
 done
 hdfs dfs -cat PR/itr_$((itr_count+1))/auth/part-00000
